b87a777fb66f18733ddee2bc80440612bf5b4622
Ryan R. Olds <ryanrolds@gmail.com>
2018-11-22 12:39:10 -0800

    Starts of templates and file reorg

--- site/site.go
+++ site/site.go
@@ -2,25 +2,47 @@ package site
 
 import(
   "fmt"
+  "io/ioutil"
   "log"
   "net/http"
+  "text/template"
 
   "github.com/ryanrolds/pedantic_orderliness/page_cache"
 )
 
 type Site struct {
   Port string
+  template *template.Template
   cache *page_cache.Cache
 }
 
-func (s *Site) Run() error {
-  s.cache = page_cache.NewCache()
+func NewSite(port string) (*Site, error) {
+  templateFile, err := ioutil.ReadFile("./site/template.html")
+  if err != nil {
+    log.Print("Cannot load ./site/template.html")
+    return nil, err
+  }
 
+  template, err := template.New("home").Parse(string(templateFile[:]))
+  if err != nil {
+    log.Print("Unable to parse ./site/template.html")
+    return nil, err
+  }
+
+  return &Site{
+    Port: port,
+    template: template,
+    cache: page_cache.NewCache(),
+  }, nil
+}
+
+func (s *Site) Run() error {
   server := http.Server{
     Addr: fmt.Sprintf(":%s", s.Port),
     Handler: s,
   }
 
+  // Blocks
   err := server.ListenAndServe()
   if err != nil {
     return err
